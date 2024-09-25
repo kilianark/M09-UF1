f2ff9d151e40bd1fe06569c3c55cefd336961c6d
Kilian Morillas <kilian08momo@gmail.com>
2024-09-25 20:22:14 +0200

    si

--- RotX.java
+++ RotX.java
@@ -1,9 +1,10 @@
-public class Rot13 {
-    public static final char[] abc = {'a','à','á','ä','ã','â','b','c','d','e','è','é','ë','ê','f','g','h','i','ì','í','ï','î','j','k','l','m','n','ñ','o','ò','ó','ö','õ','ô','p','q','r','s','t','u','ù','ú','ü','û','v','w','x','y','z'};
-    public static final int lletresAbc = abc.length;
+public class RotX {
+    public static final char[] abc = {'a','à','á','ä','ã','â','b','c','d','e','è','é','ë','ê','f','g','h','i','ì','í','ï','î','j','k','l','m','n','o','ò','ó','ö','õ','ô','p','q','r','s','t','u','ù','ú','ü','û','v','w','x','y','z'};
+    public static final int lletresAbc = 48;
+
     public static void main(String args[]) {
-        System.out.println(xifraRot13("Pe pE"));
-        System.out.println(desxifraRot13("Cr cR"));
+        System.out.println(xifraRotX("Pe pE", 3));
+        System.out.println(desxifraRotX(xifraRotX("Pe pE", 3), 3));
     }
 
     public static int buscaIndex(char lletra) {
@@ -16,14 +17,15 @@ public class Rot13 {
         }
         return index;
     }
-    public static String xifraRot13(String str) {
+
+    public static String xifraRotX(String str, int rotation) {
         String ans = "";
         for (int i = 0; i < str.length(); i++) {
             if(!Character.isLetter(str.charAt(i))) {
                 ans +=  str.charAt(i);
                 continue;
             }
-            int index = buscaIndex(str.charAt(i)) + 13;
+            int index = buscaIndex(str.charAt(i)) + rotation;
             if (index > lletresAbc-1) index -= lletresAbc;
             if(Character.isUpperCase(str.charAt(i))) ans += Character.toUpperCase(abc[index]);
             else ans += abc[index];
@@ -31,14 +33,14 @@ public class Rot13 {
         return ans;
     }
 
-    public static String desxifraRot13(String str) {
+    public static String desxifraRotX(String str, int rotation) {
         String ans = "";
         for (int i = 0; i < str.length(); i++) {
             if(!Character.isLetter(str.charAt(i))) {
                 ans +=  str.charAt(i);
                 continue;
             }
-            int index = buscaIndex(str.charAt(i)) - 13;
+            int index = buscaIndex(str.charAt(i)) - rotation;
             if (index < 0) index += lletresAbc;
             if(Character.isUpperCase(str.charAt(i))) ans += Character.toUpperCase(abc[index]);
             else ans += abc[index];
