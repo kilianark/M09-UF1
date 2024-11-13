57ec9a6f1aec64983162d2834181962fa1275ab7
Kilian Morillas <kilian08momo@gmail.com>
2024-11-13 18:19:06 +0100

    hechisima

---
+++ 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratgepk/ClauPublica.java
@@ -0,0 +1,30 @@
+package iticbcn.xifratgepk;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.Cipher;
+
+public class ClauPublica {
+    public KeyPair generaParellClausRSA() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        return keyGen.generateKeyPair();
+    }
+
+    public byte[] xifraRSA(String msg, PublicKey clauPublica) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, clauPublica);
+        return cipher.doFinal(msg.getBytes());
+    }
+
+    public String desxifraRSA(byte[] msgXifrat, PrivateKey clauPrivada) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, clauPrivada);
+        byte[] decryptedBytes = cipher.doFinal(msgXifrat);
+        return new String(decryptedBytes);
+    }
+}
